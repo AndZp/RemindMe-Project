6dc4ae9e6639de5095b025c686aa5e9eb1ab6ff4
AndZp <andrey.oleynik.zp@gmail.com>
2015-11-03 17:04:46 +0200

    Create Main Activity and Layout

---
+++ app/src/main/java/ua/com/ukrelektro/remindme/MainActivity.java
@@ -0,0 +1,15 @@
+package ua.com.ukrelektro.remindme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by User on 03.11.2015.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
