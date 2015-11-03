3878c1587b5eab1c78417ad4818f76be7ec051e0
AndZp <andrey.oleynik.zp@gmail.com>
2015-11-03 21:13:22 +0200

    Add ListView on MainActivity and create listView Item

--- app/src/main/java/ua/com/ukrelektro/remindme/ReminderObject.java
+++ app/src/main/java/ua/com/ukrelektro/remindme/ReminderObject.java
@@ -1,7 +1,48 @@
 package ua.com.ukrelektro.remindme;
 
+import java.util.Date;
+
 /**
  * Created by User on 03.11.2015.
  */
 public class ReminderObject {
+    private String title;
+    private String text;
+    private Date date;
+    private Tags tag;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Tags getTag() {
+        return tag;
+    }
+
+    public void setTag(Tags tag) {
+        this.tag = tag;
+    }
+    void test(){
+
+    }
 }
---
+++ app/src/main/java/ua/com/ukrelektro/remindme/Tags.java
@@ -0,0 +1,13 @@
+package ua.com.ukrelektro.remindme;
+
+/**
+ * Created by User on 03.11.2015.
+ */
+public enum Tags {
+
+    Work,
+    Learn,
+    Rest,
+    BirthDay;
+
+}
